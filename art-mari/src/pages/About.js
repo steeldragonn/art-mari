d1c0fe8ac3dcdba9a71e33e537bf9f0fce5060a1
steeldragonn <sweetsourr.sos@gmail.com>
2024-09-16 17:55:25 +0200

    .DS_Store deleted,backend && frontend folders are created,mongodb connected.Architecture for further implementation is done

--- art-mari/src/pages/About.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-function About() {
-  return <p> exciting style. inspiring works</p>;
-}
-export default About;
--- art-mari/src/pages/OrderList.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-function OrderList() {
-  return <p>gonna buy everything</p>;
-}
-export default OrderList;
